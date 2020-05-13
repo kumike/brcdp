95bc72237386ee1d8f785678f9b2542a7c7b5cdb
kumike <michailkubrak@gmail.com>
2020-05-13 07:03:28 +0300

    продолжаю работать над аргументами

--- prnt.py
+++ prnt.py
@@ -10,17 +10,37 @@ parser = argparse.ArgumentParser(description='A tutorial of argparse!')
 parser.add_argument("-p", action="store_true")
 parser.add_argument("-n", type=str, help="Номер штрихкода для печати")
 parser.add_argument("-c", type=int, help="Количество нужных копий штрихкода")
-#parser.add_argument("--text", type=str, help="Текст, подставится под кодом вместо цифр, небольше 24знаков")
+parser.add_argument("--text", type=str, help="Текст, подставится под кодом вместо цифр, небольше 24знаков")
 
 
 
 args = parser.parse_args()
-p = args.c
+p = args.p
+n = args.n
+c = args.c
+text = args.text
 
-p
+print('p = ',p)
+print('n = ',n)
+print('c = ',c)
+print('text = ',text)
 
-EAN = barcode.get_barcode_class('ean13')
-ean = EAN('123456789123',writer=ImageWriter)
+if type(n) is None:
+    exit() 
+else:
+    print(len(n))
+    if len(n) != 12:
+        print('Это блять нихуя ни EAN13 штрихкод! нужно токо 12 цифр! небольше неменьше!!!')
+    else:
+        print('ok!')
+
+#EAN = barcode.get_barcode_class('ean13')
+#ean = EAN(u'123456789123',writer=ImageWriter())
+
+ean = barcode.get('ean13', u'209007770000', writer=ImageWriter())
+ean.default_writer_options['text_distance'] = 2 
+ean.default_writer_options['font_size'] = 18
+ean.default_writer_options['quiet_zone'] = 6.5
 fname = ean.save('barcode')
 
 
@@ -39,10 +59,10 @@ xhtml = '''<!DOCTYPE html>
 stopA4 = 51
 i = 0
 while i <= 13: # это 13 рядов по 4 баркода, 13 строк какраз А4
-    xhtml += '<table><tr><td><img src="/home/user/barcode.png" width="167" height="88"></td>'+'\n'
-    xhtml += '<td><img src="/home/user/barcode.png" width="167" height="88" border="0"></td>'+'\n'
-    xhtml += '<td><img src="/home/user/barcode.png" width="167" height="88" border="0"></td>'+'\n'
-    xhtml += '<td><img src="/home/user/barcode.png" width="167" height="88" border="0"></td></tr></table>'+'\n'
+    xhtml += '<table><tr><td><img src="barcode.png" width="167" height="88"></td>'+'\n'
+    xhtml += '<td><img src="barcode.png" width="167" height="88" border="0"></td>'+'\n'
+    xhtml += '<td><img src="barcode.png" width="167" height="88" border="0"></td>'+'\n'
+    xhtml += '<td><img src="barcode.png" width="167" height="88" border="0"></td></tr></table>'+'\n'
     i += 1
 xhtml += '</body></html>'
 
@@ -51,18 +71,20 @@ with open('barcode.html', 'w', encoding='utf-8') as f:
 
 #print(xhtml)
 
-outfilename = "/home/user/print.pdf"
+outfilename = "print.pdf"
 resultfile = open(outfilename,'w+b')
 pdf = pisa.CreatePDF(xhtml, dest=resultfile)
 if not pdf.err:
     pdf.dest.close()
 
-
-conn = cups.Connection()
+try:
+    conn = cups.Connection()
+except RuntimeError:
+    exit()
 # Получаем список всех принтеров, подключенных к компьютеру
 printers = conn.getPrinters()
 for printer in printers: 
-    # Выводим имя принтера в консоль
+# Выводим имя принтера в консоль
     print(printer, printers[printer]["device-uri"])
 
 # Получаем первый принтер со списка принтеров
